ba9b1a5f8b9bb164449467fd290880e8cab79e7c
odarka <d.kravchenko524@gmail.com>
2023-05-24 20:13:59 +0300

    2_lesson_2tasks

--- 1/1_tasks.py
+++ 1/1_tasks.py
@@ -7,3 +7,49 @@ _price_ = "1"
 
 MY_CONSTANTA: str = '56'
 print(MY_CONSTANTA)
+
+a = 5
+b = 2
+c = a+b
+print(c)
+
+a = 5
+b = 2
+c = a-b
+print(c)
+
+a = 5
+b = 2
+c = a/b
+print(c)
+
+a = 5
+b = 2
+c = a*b
+print(c)
+
+a = 5
+b = 2
+c = a**b
+print(c)
+
+a = 5
+b = 5
+c = a//b
+print(c)
+
+a = 5
+b = 3
+c = a%b
+print(c)
+
+a = 4
+b = 12
+c = 2
+print(a*c/b)
+
+a = 4
+b = 8
+c = 16
+print(a**c/b)
+
