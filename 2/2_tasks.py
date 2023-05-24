f28b466e82f17b5cc436c51cea2bf092ae690f4f
odarka <d.kravchenko524@gmail.com>
2023-05-24 20:23:20 +0300

    2_lesson_2tasks

---
+++ 2/2_tasks.py
@@ -0,0 +1,44 @@
+a = 5
+b = 2
+c = a+b
+print(c)
+
+a = 5
+b = 2
+c = a-b
+print(c)
+
+a = 5
+b = 2
+c = a/b
+print(c)
+
+a = 5
+b = 2
+c = a*b
+print(c)
+
+a = 5
+b = 2
+c = a**b
+print(c)
+
+a = 5
+b = 5
+c = a//b
+print(c)
+
+a = 5
+b = 3
+c = a%b
+print(c)
+
+a = 4
+b = 12
+c = 2
+print(a*c/b)
+
+a = 4
+b = 8
+c = 16
+print(a**c/b)
